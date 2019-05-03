8c29e59384c889d7b4169759da1fced67b484ac5
pradeesh.kumar <pradeeshbm@ymail.com>
2019-05-03 15:51:16 +0530

    Web content  code format

--- eliteshoppy-gateway/src/main/java/com/eliteshoppy/eliteshoppygateway/EliteshoppyGatewayApplication.java
+++ eliteshoppy-gateway/src/main/java/com/eliteshoppy/eliteshoppygateway/EliteshoppyGatewayApplication.java
@@ -1,17 +1,33 @@
 package com.eliteshoppy.eliteshoppygateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
 public class EliteshoppyGatewayApplication {
+	
+	@Value("${es.gateway.crossOriginUrl}")
+	private String crossOriginUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EliteshoppyGatewayApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(crossOriginUrl);
+			}
+		};
+	}
 }
