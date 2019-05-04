325757d7e46d4006c92316e9bf4a536309f0435c
Pradeesh Bm <pradeeshbm@ymail.com>
2019-05-04 10:51:57 +0530

    UI Changed using thymeleaf

--- eliteshoppy-web/src/main/java/com/eliteshoppy/eliteshoppyweb/EliteshoppyWebApplication.java
+++ eliteshoppy-web/src/main/java/com/eliteshoppy/eliteshoppyweb/EliteshoppyWebApplication.java
@@ -3,7 +3,7 @@ package com.eliteshoppy.eliteshoppyweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.eliteshoppy.eliteshoppyweb")
 public class EliteshoppyWebApplication {
 
 	public static void main(String[] args) {
---
+++ eliteshoppy-web/src/main/java/com/eliteshoppy/eliteshoppyweb/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.eliteshoppy.eliteshoppyweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping({"/", "index.html"})
+	public String home() {
+		return "index";
+	}
+	
+	@GetMapping({"about.html"})
+	public String about() {
+		return "about";
+	}
+	
+	@GetMapping({"contact.html"})
+	public String contact() {
+		return "contact";
+	}
+	
+	@GetMapping({"mens.html"})
+	public String mens() {
+		return "mens";
+	}
+	
+	@GetMapping({"single.html"})
+	public String single() {
+		return "single";
+	}
+}
