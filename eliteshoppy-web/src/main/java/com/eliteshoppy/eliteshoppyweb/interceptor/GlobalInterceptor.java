ddc1564cd5b2b5dff2624b8befe01cf981ccaa87
Pradeesh Bm <pradeeshbm@ymail.com>
2019-05-11 09:55:00 +0530

    Code changes for config client

--- eliteshoppy-web/src/main/java/com/eliteshoppy/eliteshoppyweb/interceptor/GlobalInterceptor.java
+++ eliteshoppy-web/src/main/java/com/eliteshoppy/eliteshoppyweb/interceptor/GlobalInterceptor.java
@@ -4,11 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Component
+@RefreshScope
 public class GlobalInterceptor implements HandlerInterceptor {
 	
 	@Value("${es.api.url}")
