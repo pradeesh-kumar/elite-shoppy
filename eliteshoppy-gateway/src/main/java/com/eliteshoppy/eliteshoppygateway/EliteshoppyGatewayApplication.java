bab9f5c8433e7f6ab34576481f3e3af5e71ebdde
pradeesh.kumar <pradeeshbm@ymail.com>
2019-05-13 15:52:20 +0530

    Enabled multipart in gateway

--- eliteshoppy-gateway/src/main/java/com/eliteshoppy/eliteshoppygateway/EliteshoppyGatewayApplication.java
+++ eliteshoppy-gateway/src/main/java/com/eliteshoppy/eliteshoppygateway/EliteshoppyGatewayApplication.java
@@ -28,7 +28,8 @@ public class EliteshoppyGatewayApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*")
+				registry.addMapping("/**")
+					.allowedOrigins("*")
 					.allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 					.allowCredentials(true)
 					.allowedHeaders("*")
