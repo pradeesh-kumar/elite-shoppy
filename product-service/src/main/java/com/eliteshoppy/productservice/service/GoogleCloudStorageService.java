b988bb1536e4f9aafabd559c6a934f0b7a6cfc1a
pradeesh.kumar <pradeeshbm@ymail.com>
2019-05-17 11:39:14 +0530

    Removing access token

--- product-service/src/main/java/com/eliteshoppy/productservice/service/GoogleCloudStorageService.java
+++ product-service/src/main/java/com/eliteshoppy/productservice/service/GoogleCloudStorageService.java
@@ -1,6 +1,5 @@
 package com.eliteshoppy.productservice.service;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Base64;
@@ -10,13 +9,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.eliteshoppy.productservice.exception.StorageException;
-import com.google.api.gax.paging.Page;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import com.google.common.collect.Lists;
 import com.google.common.io.Files;
 
 @Component("storageService")
@@ -28,13 +23,13 @@ public class GoogleCloudStorageService implements StorageService {
 	private String cloudStorageBucketName;
 	@Value("${es.gcloud.cloud-storage.storage-dir}")
 	private String storageDir;
-	
+
 	private static Storage storage;
-	
+
 	static {
 		storage = StorageOptions.getDefaultInstance().getService();
 	}
-	
+
 	@Override
 	public String store(MultipartFile file) throws StorageException {
 		try {
