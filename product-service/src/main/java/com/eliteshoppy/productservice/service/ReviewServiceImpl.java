2e547281d6fd076685f7f86d0c5e5dd874aa95c1
pradeesh.kumar <pradeeshbm@ymail.com>
2019-05-28 10:34:56 +0530

    Review service bug fixes

--- product-service/src/main/java/com/eliteshoppy/productservice/service/ReviewServiceImpl.java
+++ product-service/src/main/java/com/eliteshoppy/productservice/service/ReviewServiceImpl.java
@@ -3,11 +3,13 @@ package com.eliteshoppy.productservice.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.eliteshoppy.productservice.confuguration.OAuthAuthoritiesExtractor;
 import com.eliteshoppy.productservice.model.Review;
 import com.eliteshoppy.productservice.repository.ReviewRepository;
 
+@Service
 public class ReviewServiceImpl implements ReviewService {
 
 	@Autowired private ReviewRepository reviewRepo;
